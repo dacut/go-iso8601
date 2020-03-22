c19186f59f6518dd6f165f7368ec2773f3b6aa02
David Cuthbert <dacut@kanga.org>
2020-03-22 16:29:25 -0700

    Add lowercase 't' tests. Fix README formatting.

--- iso8601_test.go
+++ iso8601_test.go
@@ -6,7 +6,10 @@ import (
 
 func TestValidDates(t *testing.T) {
 	// Regular Zulu times
-	for _, s := range []string{"1900-12-31T00:10:20Z", "1900-12-31T001020Z", "19001231T00:10:20Z", "19001231T001020Z"} {
+	for _, s := range []string{
+		"1900-12-31T00:10:20Z", "1900-12-31T001020Z", "19001231T00:10:20Z", "19001231T001020Z",
+		"1900-12-31t00:10:20Z", "1900-12-31t001020Z", "19001231t00:10:20Z", "19001231t001020Z",
+		"1900-12-31 00:10:20Z", "1900-12-31 001020Z", "19001231 00:10:20Z", "19001231 001020Z"} {
 		if ts, err := ParseISO8601Timestamp(s); err != nil {
 			t.Errorf("Failed to parse timestamp: %#v %#v\n", s, err)
 		} else if ts.Year() != 1900 || ts.Month() != 12 || ts.Day() != 31 || ts.Hour() != 0 || ts.Minute() != 10 || ts.Second() != 20 {
