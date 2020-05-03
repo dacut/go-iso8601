a6a21fbb034c82505f7111b5bedb21d736fccf3a
David Cuthbert <dacut@kanga.org>
2020-05-02 19:52:54 -0700

    Add Time type.

--- iso8601.go
+++ iso8601.go
@@ -5,8 +5,6 @@ import (
 	"regexp"
 	"strconv"
 	"time"
-
-	"github.com/palantir/stacktrace"
 )
 
 // ISO8601CompactFormat is a layout for time.Format that prints a time value
@@ -102,5 +100,152 @@ func ParseISO8601Timestamp(s string) (time.Time, error) {
 		}
 	}
 
-	return time.Time{}, stacktrace.NewError("Invalid ISO 8601 timestamp: %#v", s)
+	return time.Time{}, &time.ParseError{
+		Layout:     "ISO 8601",
+		Value:      s,
+		LayoutElem: "",
+		ValueElem:  "",
+		Message:    ": timestamp is not in ISO 8601 format",
+	}
+}
+
+// Time extends time.Time to handle valid ISO 8601 (but invalid RFC 3336)
+// formatted timestamps in UnmarshalText.
+type Time struct {
+	time.Time
+}
+
+// Date returns the Time corresponding to
+//	yyyy-mm-dd hh:mm:ss + nsec nanoseconds
+// in the appropriate zone for that time in the given location.
+//
+// The month, day, hour, min, sec, and nsec values may be outside
+// their usual ranges and will be normalized during the conversion.
+// For example, October 32 converts to November 1.
+//
+// A daylight savings time transition skips or repeats times.
+// For example, in the United States, March 13, 2011 2:15am never occurred,
+// while November 6, 2011 1:15am occurred twice. In such cases, the
+// choice of time zone, and therefore the time, is not well-defined.
+// Date returns a time that is correct in one of the two zones involved
+// in the transition, but it does not guarantee which.
+//
+// Date panics if loc is nil.
+func Date(year int, month time.Month, day, hour, min, sec, nsec int, loc *time.Location) Time {
+	return Time{time.Date(year, month, day, hour, min, sec, nsec, loc)}
+}
+
+// Now returns the current local time.
+func Now() Time {
+	return Time{time.Now()}
+}
+
+// Unix returns the local Time corresponding to the given Unix time,
+// sec seconds and nsec nanoseconds since January 1, 1970 UTC.
+// It is valid to pass nsec outside the range [0, 999999999].
+// Not all sec values have a corresponding time value. One such
+// value is 1<<63-1 (the largest int64 value).
+func Unix(sec int64, nsec int64) Time {
+	return Time{time.Unix(sec, nsec)}
+}
+
+// Add returns the time t+d.
+func (t Time) Add(d time.Duration) Time {
+	return Time{t.Time.Add(d)}
+}
+
+// AddDate returns the time corresponding to adding the
+// given number of years, months, and days to t.
+// For example, AddDate(-1, 2, 3) applied to January 1, 2011
+// returns March 4, 2010.
+//
+// AddDate normalizes its result in the same way that Date does,
+// so, for example, adding one month to October 31 yields
+// December 1, the normalized form for November 31.
+func (t Time) AddDate(years int, months int, days int) Time {
+	return Time{t.Time.AddDate(years, months, days)}
+}
+
+// In returns a copy of t representing the same time instant, but
+// with the copy's location information set to loc for display
+// purposes.
+//
+// In panics if loc is nil.
+func (t Time) In(loc *time.Location) Time {
+	return Time{t.Time.In(loc)}
+}
+
+// Local returns t with the location set to local time.
+func (t Time) Local() Time {
+	return Time{t.Time.Local()}
+}
+
+// Round returns the result of rounding t to the nearest multiple of d (since the zero time).
+// The rounding behavior for halfway values is to round up.
+// If d <= 0, Round returns t stripped of any monotonic clock reading but otherwise unchanged.
+//
+// Round operates on the time as an absolute duration since the
+// zero time; it does not operate on the presentation form of the
+// time. Thus, Round(Hour) may return a time with a non-zero
+// minute, depending on the time's Location.
+func (t Time) Round(d time.Duration) Time {
+	return Time{t.Time.Round(d)}
+}
+
+// String returns the time formatted using the RFC3339Nano string:
+//	"2006-01-02T15:04:05.999999999Z07:00"
+func (t Time) String() string {
+	return t.Format(time.RFC3339Nano)
+}
+
+// Truncate returns the result of rounding t down to a multiple of d (since the zero time).
+// If d <= 0, Truncate returns t stripped of any monotonic clock reading but otherwise unchanged.
+//
+// Truncate operates on the time as an absolute duration since the
+// zero time; it does not operate on the presentation form of the
+// time. Thus, Truncate(Hour) may return a time with a non-zero
+// minute, depending on the time's Location.
+func (t Time) Truncate(d time.Duration) Time {
+	return Time{t.Time.Truncate(d)}
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+// The time is expected to be a quoted string in any ISO 8601 format.
+func (t *Time) UnmarshalJSON(data []byte) error {
+	// Ignore null, like in the main JSON package.
+	if string(data) == "null" {
+		return nil
+	}
+
+	// Make sure the string is quoted properly.
+	s := string(data)
+	if len(s) < 2 || (!(s[0] == '"' && s[len(s)-1] == '"') && !(s[0] == '\'' && s[len(s)-1] == '\'')) {
+		return &time.ParseError{
+			Layout:     "ISO 8601",
+			Value:      s,
+			LayoutElem: "",
+			ValueElem:  "",
+			Message:    ": timestamp must be a JSON string literal",
+		}
+	}
+
+	// Remove the quotation marks.
+	s = s[1 : len(s)-1]
+
+	var err error
+	t.Time, err = ParseISO8601Timestamp(s)
+	return err
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface.
+// The time is expected to be in ISO 8601 format.
+func (t *Time) UnmarshalText(data []byte) error {
+	var err error
+	t.Time, err = ParseISO8601Timestamp(string(data))
+	return err
+}
+
+// UTC returns t with the location set to UTC.
+func (t Time) UTC() Time {
+	return Time{t.Time.UTC()}
 }
--- iso8601_test.go
+++ iso8601_test.go
@@ -1,7 +1,10 @@
 package iso8601
 
 import (
+	"encoding/json"
+	"encoding/xml"
 	"testing"
+	"time"
 )
 
 func TestValidDates(t *testing.T) {
@@ -44,7 +47,7 @@ func TestValidDates(t *testing.T) {
 	}
 
 	// Time zone conversion
-	for _, s := range []string{"2020-02-17T11:39:27.658731-02:30"} {
+	for _, s := range []string{"2020-02-17T11:39:27.658731-02:30", "2020-02-17T11:39:27.658731-0230"} {
 		if ts, err := ParseISO8601Timestamp(s); err != nil {
 			t.Errorf("Failed to parse timestamp: %#v %#v\n", s, err)
 		} else if ts.Year() != 2020 || ts.Month() != 2 || ts.Day() != 17 || ts.Hour() != 11 || ts.Minute() != 39 || ts.Second() != 27 || ts.Nanosecond() != 658731000 {
@@ -83,4 +86,112 @@ func TestInvalidDates(t *testing.T) {
 			t.Errorf("Expected an error on timestamp: %#v\n", s)
 		}
 	}
+}
+
+func TestParseJSON(t *testing.T) {
+	var jsonStruct struct {
+		Time Time `json:"time"`
+	}
+
+	for _, timeStamp := range []string{
+		"1900-12-31T00:10:20Z", "1900-12-31T001020Z", "19001231T00:10:20Z", "19001231T001020Z",
+		"1900-12-31t00:10:20Z", "1900-12-31t001020Z", "19001231t00:10:20Z", "19001231t001020Z",
+		"1900-12-31 00:10:20Z", "1900-12-31 001020Z", "19001231 00:10:20Z", "19001231 001020Z",
+		"2020-02-17T11:39:27.658731+00:00", "2020-02-17T11:39:27.658731Z", "2020-02-17T11:39:27.658731-02:30",
+	} {
+		jsonText := `{"time": "` + timeStamp + `"}`
+		if err := json.Unmarshal([]byte(jsonText), &jsonStruct); err != nil {
+			t.Errorf("Failed to unmarshall JSON: %s: %#v\n", jsonText, err)
+		}
+	}
+
+	if err := json.Unmarshal([]byte(`{"time": null}`), &jsonStruct); err != nil {
+		t.Errorf("Failed to unmarhsall null time")
+	}
+
+	err := json.Unmarshal([]byte(`{"time": 1}`), &jsonStruct)
+	if err == nil {
+		t.Errorf("Expected a time.ParseError")
+	} else if _, ok := err.(*time.ParseError); !ok {
+		t.Errorf("Expected a time.ParseError")
+	}
+}
+
+func TestParseText(t *testing.T) {
+	var xmlAttrStruct struct {
+		Time Time `xml:"time,attr"`
+	}
+
+	var xmlEmbedStruct struct {
+		Time Time `xml:"time"`
+	}
+
+	for _, timeStamp := range []string{
+		"1900-12-31T00:10:20Z", "1900-12-31T001020Z", "19001231T00:10:20Z", "19001231T001020Z",
+		"1900-12-31t00:10:20Z", "1900-12-31t001020Z", "19001231t00:10:20Z", "19001231t001020Z",
+		"1900-12-31 00:10:20Z", "1900-12-31 001020Z", "19001231 00:10:20Z", "19001231 001020Z",
+		"2020-02-17T11:39:27.658731+00:00", "2020-02-17T11:39:27.658731Z", "2020-02-17T11:39:27.658731-02:30",
+	} {
+		xmlAttrText := `<value time="` + timeStamp + `" />`
+		xmlEmbedText := `<value><time>` + timeStamp + `</time></value>`
+
+		if err := xml.Unmarshal([]byte(xmlAttrText), &xmlAttrStruct); err != nil {
+			t.Errorf("Failed to unmarshall XML: %s: %#v\n", xmlAttrText, err)
+		}
+
+		if err := xml.Unmarshal([]byte(xmlEmbedText), &xmlEmbedStruct); err != nil {
+			t.Errorf("Failed to unmarshall XML: %s: %#v\n", xmlEmbedText, err)
+		}
+	}
+
+}
+
+func TestTimeCompilation(t *testing.T) {
+	// This just makes sure the iso8601.Time type is returned from various
+	// methods properly.
+	oneDay, _ := time.ParseDuration("24h")
+	oneMinute, _ := time.ParseDuration("1m")
+	start := Date(2001, 1, 1, 3, 15, 45, 0, time.FixedZone("UTC", 0))
+	tomorrow := start.Add(oneDay)
+	zone333 := time.FixedZone("333", -(3*3600 + 33*60))
+
+	if !tomorrow.After(start.Time) {
+		t.Errorf("Expected tomorrow to be after today: tomorrow=%v start=%v oneDay=%v\n", tomorrow, start, oneDay)
+	}
+
+	dayAfterTomorrow := start.AddDate(0, 0, 2)
+	if !dayAfterTomorrow.After(tomorrow.Time) {
+		t.Errorf("Expected dayAfterTomorrow to be after tomorrow")
+	}
+
+	rounded := start.Round(oneMinute)
+	truncated := start.Truncate(oneMinute)
+
+	roundedDiff := rounded.Sub(start.Time)
+	if roundedDiff != 15000000000 {
+		t.Errorf("Expected rounded to be 15 seconds after start")
+	}
+
+	truncatedDiff := start.Sub(truncated.Time)
+	if truncatedDiff != 45000000000 {
+		t.Errorf("Expected truncated to be 45 seconds before start")
+	}
+
+	localStart := start.Local()
+	start.Sub(localStart.Time)
+
+	startUTC := localStart.UTC()
+	startUTCDiff := startUTC.Sub(start.Time)
+	if startUTCDiff != 0 {
+		t.Errorf("Expected startUTC to equal start: startUTC=%v start=%v", startUTC, start)
+	}
+
+	start333 := start.In(zone333)
+	diff333 := start.Sub(start333.Time)
+	if diff333 != 0 {
+		t.Errorf("Expected start and start333 to be the same: start=%v start333=%v", start, start333)
+	}
+
+	Now().Sub(time.Now())
+	Unix(0, 0).Sub(time.Unix(0, 0))
 }
